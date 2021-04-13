84b41cbd74418f3b5d0e2c54ffb928da4feb1418
Duncan Lovett <duncan@flairconsultancy.com>
2021-04-13 10:59:43 +0100

    Removed classesfrom HTML, reset theme.scss

--- assets/js/theme/global/quick-view.js
+++ assets/js/theme/global/quick-view.js
@@ -3,8 +3,6 @@ import 'foundation-sites/js/foundation/foundation.dropdown';
 import utils from '@bigcommerce/stencil-utils';
 import ProductDetails from '../common/product-details';
 import { defaultModal } from './modal';
-import 'slick-carousel';
-import { onSlickCarouselChange, onUserCarouselChange } from '../common/carousel';
 
 export default function (context) {
     const modal = defaultModal();
@@ -20,16 +18,6 @@ export default function (context) {
             modal.updateContent(response);
 
             modal.$content.find('.productView').addClass('productView--quickView');
-
-            const $carousel = modal.$content.find('[data-slick]');
-
-            if ($carousel.length) {
-                $carousel.on('init afterChange', (e, carousel) => onSlickCarouselChange(e, carousel, context));
-                $carousel.on('click', '.slick-arrow, .slick-dots', $carousel, e => onUserCarouselChange(e, context));
-                $carousel.on('swipe', (e, carouselObj) => onUserCarouselChange(e, context, carouselObj.$slider));
-
-                $carousel.slick();
-            }
 
             modal.setupFocusTrap();
 
